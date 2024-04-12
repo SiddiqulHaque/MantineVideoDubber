3f0a206bcd27baf4911e1a06cee4a593fcd9559d
Siddiqul-haque <siddiqamu786@gmail.com>
2024-04-12 11:45:24 +0530

    some changes

--- src/components/TableSelection.tsx
+++ src/components/TableSelection.tsx
@@ -80,7 +80,7 @@ export default function TableSelection() {
 
   return (
     <ScrollArea>
-      <Table miw={1100} verticalSpacing="sm">
+      <Table className='min-w-full' verticalSpacing="sm">
         <Table.Thead>
           <Table.Tr>
             <Table.Th style={{ width: rem(40) }}>
